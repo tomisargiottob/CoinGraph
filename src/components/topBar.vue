8ea520baa13aae4694f6cbc1f1dd785648f27929
tomisargiottob <tomisargiottob@hotmail.com>
2022-08-27 18:06:05 +0200

    refactor(topbar): refactor top bar functionality and appearence

--- src/components/topBar.vue
+++ src/components/topBar.vue
@@ -1,61 +1,105 @@
 <template>
 	<el-menu
 	:default-active="activeIndex"
-	class="el-menu-demo"
 	mode="horizontal"
 	:ellipsis="false"
 	text-color="black"
 	@select="handleSelect"
 	>
-	<el-menu-item index="0">
-		<el-image src="./coinGraphMin.png"/>
-	</el-menu-item>
-	<div class="flex-grow" />
-	<el-menu-item index="1">Dashboard</el-menu-item>
-	<el-sub-menu index="2">
-      <template #title>Perfil</template>
-      <!-- <el-menu-item index="2-1">Información</el-menu-item> -->
-      <el-menu-item index="2-2">Configuración</el-menu-item>
-		<hr>
-      <el-menu-item index="2-3">Cerrar Sesión</el-menu-item>
-    </el-sub-menu>	
+  <el-row class="main-menu"> 
+    <el-col :xs="authStore.authenticated ? 24 : 11" :sm="authStore.authenticated ? 24 : 14" :md="authStore.authenticated ? 24 : 14" :lg="authStore.authenticated ? 24 : 16" :xl="authStore.authenticated ? 24 : 16">
+      <el-row>
+        <el-col :span="!authStore.authenticated ? 24 : 10">
+          <el-menu-item index="0">
+            <el-image src="./coinGraphMin.png"/>
+          </el-menu-item>
+        </el-col>
+        <el-col v-if="authStore.authenticated" :span="14">
+          <el-row justify="end">
+            <el-menu-item index="1">Dashboard</el-menu-item>
+            <el-sub-menu index="2">
+              <template #title>Perfil</template>
+              <el-menu-item index="2-2">Configuración</el-menu-item>
+            <hr>
+              <el-menu-item index="2-3">Cerrar Sesión</el-menu-item>
+            </el-sub-menu>	
+          </el-row>
+        </el-col>
+      </el-row>
+    
+    </el-col>
+    <el-col v-if="!authStore.authenticated && !currentRoute" class="session-button" :xs="6" :sm="4">
+      <el-button type="primary" plain class="log-in" @click="redirectTo('Login')"> Ingresar </el-button>
+    </el-col>
+    <el-col v-if="!authStore.authenticated && !currentRoute" class="session-button" :xs="7" :sm="4">
+      <el-button type="primary" @click="redirectTo('Register')"> Registrarse </el-button>
+    </el-col>
+
+  </el-row>
+
   </el-menu>
+  
 </template>
 
 	
 <script setup>
 	import { useAuthStore } from "../store/authStore";
 	import { useRouter } from 'vue-router'
-	import { ref } from 'vue'
+	import { ref, watch } from 'vue'
 
 	const activeIndex = ref('1')
-    const router = useRouter();
-    const authStore = useAuthStore();
+  const router = useRouter();
+  const authStore = useAuthStore();
+  const redirectTo = (page) => {
+    router.push({ name: page });
+  }
+  const currentRoute = ref(false);
+  watch(router.currentRoute,(value) => {
+    currentRoute.value = ['Login', 'Register'].includes(value.name);
+  });
 	const handleSelect = async (key) => {
-    if (key === '1') {
-       router.push({ name: 'Home' })
+    if (key === '0') {
+      redirectTo('Landing');
+    } else if (key === '1') {
+      redirectTo('Home');
     } else if (key === '2-2') {
-			router.push({ name: 'Profile' })
+			redirectTo('Profile');
 		} else if (key === '2-3') {
 			await authStore.logoutUser();
-      router.push({ name: 'Login' })
+      redirectTo('Login')
 		}
 	}	
 </script>
 
-<style>
-.el-image__inner{
-	height: 50px;
-}
-.flex-grow {
-  flex-grow: 1;
-}
+<style lang="scss">
 .el-menu-item, 
 .el-sub-menu,
 .el-sub-menu__title {
-	font-size: 1rem;
+  font-size: 1.2rem;
+  min-width: 80px;
+  padding: 0px 2%;
+  // font-weight: 600;
+  .is-active, &:hover, &:focus{
+    background-color: white !important;
+  }
 }
-/* .el-menu{
-	margin-bottom:20px
-} */
+.main-menu {
+  width:100%;
+  padding: 10px 20px;
+  .el-image__inner{
+    height: 50px;
+  }
+  .flex-grow {
+    flex-grow: 1;
+  }
+  .session-button{
+    padding: 10px;
+    padding-top: 15px;
+    .log-in{
+      border: 2px solid #409eff;
+      font-weight: 700;
+    }
+  }
+
+}
 </style>
