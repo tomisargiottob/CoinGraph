d84022c6d52c70af670bfeaa58bdd147a4c0035d
tomisargiottob <tomisargiottob@hotmail.com>
2022-09-04 12:38:59 +0200

    fix(router): scroll to top on each page change

--- src/router/router.js
+++ src/router/router.js
@@ -24,6 +24,7 @@ export const router = createRouter({
 })
 
 router.beforeEach((to, from, next) => {
+  window.scrollTo(0, 0)
 	const publicPages = ['/login', '/register'];
   const authData = useAuthStore();
   if(to.path === '/') {
